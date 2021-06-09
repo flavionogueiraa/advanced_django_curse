744547121d8b2ebe7187574dd79ebc83361c1f14
flaviotech <flavio981895788@gmail.com>
2021-06-09 19:04:11 -0300

    Modulo 3 - Aula 4

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,11 +2,13 @@
 Favor colocar as importações em ordem alfabética para uma melhor organização
 '''
 
-from .views import person_create
-from .views import persons_list
+from .views import person_form
+from .views import person_list
+from .views import person_update
 from django.urls import path
 
 urlpatterns = [
-    path('create/', person_create, name='person_create'),
-    path('list/', persons_list, name='persons_list'),
+    path('create/', person_form, name='person_form'),
+    path('list/', person_list, name='person_list'),
+    path('update/<int:id>/', person_update, name='person_update'),
 ]

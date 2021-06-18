78942385e065df612ab555d89349c7f46b00ee1c
flaviotech <flavio981895788@gmail.com>
2021-06-17 21:02:38 -0300

    Modulo 2 - Aula 2

--- home/views/__init__.py
+++ home/views/__init__.py
@@ -5,9 +5,11 @@ Favor colocar as importações em ordem alfabética para uma melhor organizaçã
 from .home import home
 from .home3 import Home3
 from .logout import logout_view
+from .view import MyView
 
 __all__ = [
     home,
     Home3,
     logout_view,
+    MyView,
 ]
---
+++ home/views/view.py
@@ -0,0 +1,19 @@
+'''
+Favor colocar as importações em ordem alfabética para uma melhor organização
+'''
+
+from django.shortcuts import HttpResponse, render
+from django.views.generic.base import View
+
+class MyView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        
+        return render(
+            request,
+            'view.html',
+            context
+        )
+    
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('Post')

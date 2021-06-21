a13fc894971f06f93f12dda758e5f4b05f4b0671
flaviotech <flavio981895788@gmail.com>
2021-06-21 18:49:02 -0300

    Modulo 2 - Aula 4 - ADV

--- clientes/urls.py
+++ clientes/urls.py
@@ -3,6 +3,7 @@ Favor colocar as importações em ordem alfabética para uma melhor organizaçã
 '''
 
 from .views import person_delete
+from .views import PersonDetailView
 from .views import person_form
 from .views import person_list
 from .views import PersonList
@@ -17,4 +18,5 @@ urlpatterns = [
     path('delete/<int:id>/', person_delete, name='person_delete'),
 
     path('list_cbv/', login_required(PersonList.as_view()), name='person_list_cbv'),
+    path('detail_cbv/<int:pk>/', login_required(PersonDetailView.as_view()), name='person_detail_cbv'),
 ]

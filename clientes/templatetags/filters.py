dce50e1a3277b88b46544cad08f2e0921d72360a
flaviotech <flavio981895788@gmail.com>
2021-06-29 21:41:24 -0300

    modulo 3 - Aula 3 - ADV

---
+++ clientes/templatetags/filters.py
@@ -0,0 +1,7 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def my_first_filter(data):
+    return '{} - Meu primeiro filtro'.format(data)
--- clientes/views/person_detail.py
+++ clientes/views/person_detail.py
@@ -3,13 +3,7 @@ Favor colocar as importações em ordem alfabética para uma melhor organizaçã
 '''
 
 from ..models import Person
-from datetime import date
 from django.views.generic.detail import DetailView
 
 class PersonDetailView(DetailView):
     model = Person
-
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['today'] = date.today()
-        return context

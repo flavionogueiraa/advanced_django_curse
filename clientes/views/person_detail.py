65462cf350e9826e3052bbf3bd560873f0a70ec4
flaviotech <flavio.nogueira.profissional@gmail.com>
2021-11-12 19:53:48 -0300

    Modulo 4 - Aula 2 - ADV

--- clientes/views/person_detail.py
+++ clientes/views/person_detail.py
@@ -2,8 +2,26 @@
 Shift + Alt + O para organizar as importações (vs code)
 '''
 
-from ..models import Person
 from django.views.generic.detail import DetailView
+
+from ..models import Person, Venda
+
 
 class PersonDetailView(DetailView):
     model = Person
+
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        return Person.objects.select_related(
+            'documento'
+        ).get(pk=pk)
+    
+    def get_context_data(self, **kwargs):
+        context = super(PersonDetailView, self).get_context_data(**kwargs)
+
+        context['vendas'] = Venda.objects.filter(
+            pessoa=self.object
+        )
+
+        return context
+    

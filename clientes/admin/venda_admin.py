30f3e87ef7ae9f73d8eb7ebd09fd19009d5ceb3f
flaviotech <flavio.nogueira.profissional@gmail.com>
2021-11-20 09:12:53 -0300

    Modulo 4 - Aula 7 - ADV

--- clientes/admin/venda_admin.py
+++ clientes/admin/venda_admin.py
@@ -2,8 +2,10 @@
 Shift + Alt + O para organizar as importações (vs code)
 '''
 
-from ..models import Venda
 from django.contrib import admin
+
+from ..models import Venda
+
 
 @admin.register(Venda)
 class VendaAdmin(admin.ModelAdmin):

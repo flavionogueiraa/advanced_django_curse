527e2c7eb7624d3fe6d3f1c4308a393a3b335676
flaviotech <flavio.nogueira.profissional@gmail.com>
2021-11-27 20:30:05 -0300

    Módulo 6 - Aula 4 - ADV

--- vendas/views/dashboard_view.py
+++ vendas/views/dashboard_view.py
@@ -2,7 +2,7 @@
 Shift + Alt + O para organizar as importações (vs code)
 '''
 
-from django.db.models import Avg
+from django.db.models import Avg, Max, Min
 from django.shortcuts import render
 from django.views import View
 
@@ -11,12 +11,35 @@ from ..models import Venda
 
 class DashboardView(View):
     def get(self, request):
-        media = Venda.objects.all().aggregate(
-            valor_total=Avg('total')
-        )['valor_total'] or 0
+        media_valor = Venda.objects.all().aggregate(
+            valor_medio=Avg('total')
+        )['valor_medio'] or 0
         
+        media_desconto = Venda.objects.all().aggregate(
+            valor_medio=Avg('desconto')
+        )['valor_medio'] or 0
+
+        menor_venda = Venda.objects.all().aggregate(
+            menor_valor=Min('total')
+        )['menor_valor'] or 0
+
+        maior_venda = Venda.objects.all().aggregate(
+            maior_valor=Max('total')
+        )['maior_valor'] or 0
+
+        total_vendas = Venda.objects.all().count()
+        
+        total_vendas_com_nota_fiscal_emitida = Venda.objects.filter(
+            nota_fiscal_emitida=True
+        ).count()
+
         context = {
-            'media': media,
+            'media_valor': media_valor,
+            'media_desconto': media_desconto,
+            'menor_venda': menor_venda,
+            'maior_venda': maior_venda,
+            'total_vendas': total_vendas,
+            'total_vendas_com_nota_fiscal_emitida': total_vendas_com_nota_fiscal_emitida
         }
         
         return render(

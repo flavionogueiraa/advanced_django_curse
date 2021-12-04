4de601ff47db1d066eacbcbe6b086ed9e07ee3eb
flaviotech <flavio.nogueira.profissional@gmail.com>
2021-12-04 13:38:40 -0300

    Módulo 9 - Aula 3 - ADV

--- clientes/views/person_list_cbv.py
+++ clientes/views/person_list_cbv.py
@@ -10,3 +10,30 @@ from ..models import Person
 
 class PersonList(LoginRequiredMixin, ListView):
     model = Person
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cria_mensagem_bem_vindo(self.request, context)
+        return context
+
+
+
+#ANCHOR funções auxiliares
+def cria_mensagem_bem_vindo(request, context):
+    ja_acessou = request.session.get('ja_acessou', None)
+    if ja_acessou:
+        context.update({
+            'mensagem': 'Olá {}, bem vindo de volta!'.format(request.user)
+        })
+    else:
+        context.update({
+            'mensagem': 'Olá {}, bem vindo ao nosso sistema pela primeira vez!'.format(request.user)
+        })
+    
+    atualiza_ja_acessou(request, ja_acessou)
+
+def atualiza_ja_acessou(request, ja_acessou):
+    if not ja_acessou:
+        request.session.update({
+            'ja_acessou': True
+        })

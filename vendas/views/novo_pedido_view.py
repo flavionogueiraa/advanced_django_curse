88fb22d39119499dd8cbfb5c94b52f375fce35a4
flaviotech <flavio.nogueira.profissional@gmail.com>
2021-12-15 14:40:54 -0300

    Módulo 12 - Aula 2 - ADV

--- vendas/views/novo_pedido_view.py
+++ vendas/views/novo_pedido_view.py
@@ -2,9 +2,13 @@
 Shift + Alt + O para organizar as importações (vs code)
 '''
 
+from decimal import Decimal
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic.base import View
+
+from ..models import Venda
 
 
 class NovoPedidoView(LoginRequiredMixin, View):
@@ -15,7 +19,23 @@ class NovoPedidoView(LoginRequiredMixin, View):
         )
 
     def post(self, request):
+        context = {}
+        context['numero_venda'] = request.POST.get('numero_venda', None)
+        context['desconto'] = request.POST.get('desconto', None)
+        context['venda_id'] = request.POST.get('venda_id', None)
+
+        if context['venda_id']:
+            context['venda'] = get_object_or_404(Venda, pk=context['venda_id'])
+        else:
+            context['venda'] = Venda.objects.create(
+                numero_venda=context['numero_venda'],
+                desconto=Decimal(context['desconto'])
+            )
+        context['venda_id'] = context['venda'].id
+
+        
         return render(
             request,
-            'vendas/novo_pedido.html'
+            'vendas/novo_pedido.html',
+            context
         )

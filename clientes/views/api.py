ed132316c0a66d0e7a81ef058d72a42561c63cbb
flaviotech <flavio.nogueira.profissional@gmail.com>
2021-12-18 09:07:55 -0300

    Módulo 13 - Aula 2 - ADV

--- clientes/views/api.py
+++ clientes/views/api.py
@@ -3,7 +3,9 @@ Shift + Alt + O para organizar as importações (vs code)
 '''
 
 from django.contrib.auth.decorators import login_required
+from django.forms.models import model_to_dict
 from django.http.response import JsonResponse
+from produtos.models import Produto
 
 
 @login_required
@@ -13,7 +15,8 @@ def api(request):
         'idade': '30',
         'salario': 1000.00
     }
-
     lista = [1, 2, 3, 4, 5]
+    produto = Produto.objects.last()
+    lista_produto = [model_to_dict(produto) for produto in Produto.objects.all()]
 
-    return JsonResponse(lista, safe=False)
+    return JsonResponse(lista_produto, safe=False)
